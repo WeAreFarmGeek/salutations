aab255344f9e6988e38393c9b7368e6dd38c70d1
John Hamelink <john@johnhamelink.com>
2015-06-17 11:08:32 +0100

    Improved salutations list, added ability to select salutations by theme

--- lib/salutations.rb
+++ lib/salutations.rb
@@ -1,17 +1,18 @@
 class Salutations
+
+  COMMON   = ["Mr", "Master", "Ms", "Miss", "Mrs", "Mx"]
+  SCIENCE  = ["Dr", "Prof"]
+  FORMAL   = ["Sir", "Madam", "Dame", "Lord", "Lady", "Viscount"]
+  RELIGION = ["Br", "Sr", "Fr", "Rev", "Pr", "Elder", "Revd Father", "Revd Canon"]
+  POLITICS = ["The Hon", "The Hon Mrs", "Rt Hon Lord"]
+  LAW      = ["Judge"]
+  MILITARY = [
+    "Capt", "Major", "Lt-Col", "Col", "Lt-Cmdr",
+    "Cmdr", "Flt Lt", "Brgdr", "Wng Cmdr", "Group Capt", 
+    "Maj-Gen", "Air Cdre", "Rear Admrl"
+  ]
+
   def self.get
-    [
-      "Mr.", "Mrs.", "Miss",
-      "Dr.", "Ms.", "Prof.",
-      "Rev.", "Lady", "Sir",
-      "Capt.", "Major", "Lt.-Col.",
-      "Col.", "Lady", "Lt.-Cmdr.",
-      "The Hon.", "Cmdr.", "Flt. Lt.",
-      "Brgdr.", "Judge", "Lord",
-      "The Hon. Mrs", "Wng. Cmdr.", "Group Capt.",
-      "Rt. Hon. Lord", "Revd. Father", "Revd Canon",
-      "Maj.-Gen.", "Air Cdre.", "Viscount",
-      "Dame", "Rear Admrl."
-    ]
+    [COMMON, SCIENCE, FORMAL, RELIGION, POLITICS, LAW, MILITARY].flatten.uniq
   end
 end
--- lib/salutations/version.rb
+++ lib/salutations/version.rb
@@ -1,3 +1,3 @@
 module Salutations
-  VERSION = "0.0.1"
+  VERSION = "0.1.0"
 end
